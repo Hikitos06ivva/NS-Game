97d8bc321b9f63ebc48cca050477a29b5009a04e
Hikitos06ivva <nikitr1game1pro@gmail.com>
2024-02-07 14:30:33 +0300

    0.1
    Первая рабочая версия игры. Персонаж умеет передвигаться, пока без анимаций

---
+++ First Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForse = 10f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+
+    }
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f)
+            rb.AddForce(new Vector2(0, jumpForse), ForceMode2D.Impulse);
+
+        sprite.flipX = movement < 0 ? true : false;
+    }
+}
